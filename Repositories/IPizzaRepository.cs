6e3977a710df3ef6ad52890eaec19361a065ba37
Rozanov Ivan <navi.zor9@gmail.com>
2022-11-16 17:18:38 +0300

    Add IPizzaRepository interface

---
+++ Repositories/IPizzaRepository.cs
@@ -0,0 +1,13 @@
+﻿using PizzaWebApi.Models;
+
+namespace PizzaWebApi.Repositories
+{
+    public interface IPizzaRepository : IDisposable
+    {
+        public Task<IEnumerable<Pizza>> Get();
+        public Task<Pizza?> Get(int id);
+        public Task<Pizza> Create(Pizza pizza);
+        public Task Update(Pizza pizza);
+        public Task Delete(int id);
+    }
+}
