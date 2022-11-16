139d30363fbec785d62e92fd2fc7cb708942da3c
Rozanov Ivan <navi.zor9@gmail.com>
2022-11-16 17:23:32 +0300

    add Pizza repository wich implements IPizzaRepository intarface

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<PizzaContext>(options => options.UseSqlite(connection));
 
---
+++ Repositories/PizzaRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaWebApi.Models;
+
+namespace PizzaWebApi.Repositories
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        private readonly PizzaContext _context;
+
+        public PizzaRepository(PizzaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Pizza> Create(Pizza pizza)
+        {
+            await using (_context)
+            {
+                await _context.Pizzas.AddAsync(pizza);
+                await _context.SaveChangesAsync();
+                return pizza;
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            await using (_context)
+            {
+                var book = await _context.Pizzas.FindAsync(id);
+                if (book is not null)
+                {
+                    _context.Pizzas.Remove(book);
+                    await _context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task<IEnumerable<Pizza>> Get()
+        {
+            await using (_context)
+            {
+                return await _context.Pizzas.ToListAsync();
+            }
+        }
+
+        public async Task<Pizza?> Get(int id)
+        {
+            await using (_context)
+            {
+                return await _context.Pizzas.FindAsync(id);
+            }
+        }
+
+        public async Task Update(Pizza pizza)
+        {
+            await using (_context)
+            {
+                _context.Pizzas.Update(pizza);
+                await _context.SaveChangesAsync();
+            }
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed && disposing)
+            {
+                _context.Dispose();
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
